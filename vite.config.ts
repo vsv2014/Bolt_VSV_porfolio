06ea7b1b635052e52d0c8b6ae6031c7d99797b13
vsv2014 <santhoshvishal.v@research.iiit.ac.in>
2025-03-13 12:22:35 +0530

    fix: Revert to absolute paths for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/Bolt_VSV_porfolio/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
